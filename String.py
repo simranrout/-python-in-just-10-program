5c5f1d01d2c31486aac1999f31f1ec50fedfcd43
simranrout <43809488+simranrout@users.noreply.github.com>
2019-08-29 20:35:42 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,24 @@
+#in this section we will declare a string.....
+# we can  use "" and '' ....
+# here we will use slicing to get a specific part....
+x='python'
+x1=x[3]
+print(x1) #it will print letter which is on the 3rd index.......as it starts from 0th index then 1st then 2nd index....
+x2=x[0:3]
+print(x2)#it will print from 0th index upto 3rd index i.e. 0th then 1st then 2nd....
+x3=x[:3]
+print(x3)#it will print from starting upto 3rd index...
+x4=x[3:]
+print(x4)# from 3rd index to last
+
+"""to print a line like this
+	heyy its me "simran" nice to meet you all..
+	we can do like the following program:-
+"""
+print("heyy its me \"simran\" nice to meet you")
+
+
+l1="Alligator"
+
+print(l1[:4])
+print(l1[6:])
---
+++ Varribles.py
@@ -0,0 +1,38 @@
+#declarartion of varriable and operations on it......
+#we declare varriable without mentioning the datatype.....
+#In python we can directly add 2 different datatype value without datatype
+a= 15
+a1= 15
+a2= 15
+a3= 15
+a4= 15
+b='heyy'
+c=9.1
+d=6
+add=a+c  
+add2=c+d
+sub=c-d
+sub2=c-a
+mul=c*d
+div=c/d
+
+print(add,add2,b,sub,sub2,mul,div)
+
+#we also can directly add it and print it
+op=12+0.1
+print(op)
+
+#special operations
+a += 23 #it will directly add 15 with 23 as a value is 15
+print(a)
+print("current value of a  ",a1)
+a1**= 2	 #it prints 2 times of a1 i.e. 15*15......
+print(a1)
+a2 **=3 #it prints 3 times of a1 i.e. 15*15*15......
+print(a2)
+print("value of a  ",a1)
+a3 //=1 # give quotient
+print(a3)
+a4 %= 1	#give reminder
+print(a4)
+print(a)
